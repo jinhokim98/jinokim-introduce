34a063a4553fad98a701d56de937a334069ecdad
김진호 <rlawlsgh1227@gmail.com>
2024-09-01 21:24:50 +0900

    style: 600px 미만 반응형 설정

--- src/components/overview/index.tsx
+++ src/components/overview/index.tsx
@@ -10,28 +10,26 @@ const Overview = () => {
 
   return (
     <article className={styles.layout}>
-      <header className={styles.header}>
-        <hgroup className={styles.hgroup}>
-          <h1 className={`${styles.name} primary_color`}>김진호</h1>
-          <h2 className="subtitle gray_color">Frontend developer</h2>
-        </hgroup>
-        <aside className={styles.followMe} aria-label="follow me">
-          <nav className={styles.buttonGroup}>
-            <ButtonIcon Icon={Instagram} onClick={() => openSite('https://www.instagram.com/jinokim98/')} />
-            <ButtonIcon Icon={Github} onClick={() => openSite('https://github.com/jinhokim98')} />
-          </nav>
-          <p className={`${styles.p} paragraph`}>
-            <span className="label">{'Blog: '}</span>
-            <a href="https://jinokim.tistory.com/" target="_blank_">
-              Jinokim의 개발일지
-            </a>
-          </p>
-          <p className={`${styles.p} paragraph`}>
-            <span className="label">{'Email: '}</span>
-            rlawlsgh1227@gmail.com
-          </p>
-        </aside>
-      </header>
+      <hgroup className={styles.hgroup}>
+        <h1 className={`${styles.name} primary_color`}>김진호</h1>
+        <h2 className="subtitle gray_color">Frontend developer</h2>
+      </hgroup>
+      <aside className={styles.followMe} aria-label="follow me">
+        <nav className={styles.buttonGroup}>
+          <ButtonIcon Icon={Instagram} onClick={() => openSite('https://www.instagram.com/jinokim98/')} />
+          <ButtonIcon Icon={Github} onClick={() => openSite('https://github.com/jinhokim98')} />
+        </nav>
+        <p className={`${styles.p} paragraph`}>
+          <span className="label">{'Blog: '}</span>
+          <a href="https://jinokim.tistory.com/" target="_blank_">
+            Jinokim의 개발일지
+          </a>
+        </p>
+        <p className={`${styles.p} paragraph`}>
+          <span className="label">{'Email: '}</span>
+          rlawlsgh1227@gmail.com
+        </p>
+      </aside>
     </article>
   );
 };
--- src/components/projects/haengdong/details/frontend/index.tsx
+++ src/components/projects/haengdong/details/frontend/index.tsx
@@ -42,9 +42,7 @@ const Frontend = () => {
               <p className="paragraph">디자인은 haengdong-design 라이브러리를 통해 관리</p>
             </Li>
             <Li>
-              <p className="paragraph">
-                데이터는 서버, 클라이언트 상태 관리 라이브러리를 활용해서 관리 후 디자인 시스템에 주입
-              </p>
+              <p className="paragraph">데이터는 상태 관리 라이브러리를 활용해서 관리 후 디자인 시스템에 주입</p>
             </Li>
             <Li>
               <p className="paragraph">기능은 custom hook에 데이터를 넘겨줘서 처리</p>
@@ -52,8 +50,8 @@ const Frontend = () => {
           </Ul>
           <Li>
             <p className="paragraph">
-              각각의 관심사를 분리하여 코드를 작성함으로써, 변화가 일어났을 때 그 성격에 맞는 부분만 수정해주면 되는
-              구조로 만들어 변화에 유연하게 대응할 수 있도록 했습니다.
+              각각의 관심사를 분리하여 코드를 작성, 변화가 일어났을 때 그 부분만 수정해주면 되는 구조로 변화에 유연하게
+              대응할 수 있도록 했습니다.
             </p>
           </Li>
         </Ul>
