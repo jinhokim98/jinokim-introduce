483c69f8d9354f281b46971889c0945bb30d63c8
김진호 <rlawlsgh1227@gmail.com>
2024-09-07 23:35:33 +0900

    feat: 계좌번호에 금액 추가

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -37,7 +37,7 @@ const Test = () => {
   };
 
   const goToss = async () => {
-    const account = '국민은행 030302-04-191806 김진호';
+    const account = '국민은행 030302-04-191806 김진호 10,000원';
     await window.navigator.clipboard.writeText(account);
 
     const url = 'supertoss://';
