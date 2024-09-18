34a8f992245a0b8fc0b5115b6130837b944881a4
김진호 <rlawlsgh1227@gmail.com>
2024-09-18 20:17:19 +0900

    style: 불필요한 코드 제거

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -21,8 +21,6 @@ const Test = () => {
   const [buttonText, setButtonText] = useState('공유하기 전');
 
   const shareLink = async () => {
-    console.log();
-
     if (typeof window.navigator.share === 'undefined' || !window.navigator.canShare(shareObject)) {
       alert('OS 공유를 할 수 없는 환경입니다.');
       return;
