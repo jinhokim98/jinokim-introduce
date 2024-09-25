79f1531af3801e8d53d763cff5570e4b4be5d246
김진호 <rlawlsgh1227@gmail.com>
2024-09-26 01:27:43 +0900

    feat: 스킬 수정

--- src/components/skills/index.tsx
+++ src/components/skills/index.tsx
@@ -1,79 +1,41 @@
-import {Li, Ul} from '@components/common/list';
 import PageTitle from '@components/common/pageTitle';
+import styles from './style.module.css';
 
 const Skills = () => {
   return (
     <article className="page_layout">
       <PageTitle title="Skills" />
-      <section className="label" aria-label="Language">
-        <p>Language</p>
-        <Ul>
-          <Li>
-            <p className="paragraph">Javascript</p>
-          </Li>
-          <Li>
-            <p className="paragraph">Typescript</p>
-          </Li>
-        </Ul>
-      </section>
-      <section className="label" aria-label="Framework">
-        <p>Framework</p>
-        <Ul>
-          <Li>
-            <p className="paragraph">React.js</p>
-          </Li>
-        </Ul>
-      </section>
-      <section className="label" aria-label="Bundler">
-        <p>Bundler</p>
-        <Ul>
-          <Li>
-            <p className="paragraph">Webpack</p>
-          </Li>
-          <Li>
-            <p className="paragraph">Vite</p>
-          </Li>
-        </Ul>
-      </section>
-      <section className="label" aria-label="Style library">
-        <p>Style library</p>
-        <Ul>
-          <Li>
-            <p className="paragraph">Emotion.js</p>
-          </Li>
-          <Li>
-            <p className="paragraph">Styled components</p>
-          </Li>
-        </Ul>
-      </section>
-      <section className="label" aria-label="State management library">
-        <p>State management library</p>
-        <Ul>
-          <Li>
-            <p className="paragraph">Zustand, Recoil</p>
-          </Li>
-          <Li>
-            <p className="paragraph">React-Query</p>
-          </Li>
-        </Ul>
-      </section>
-      <section className="label" aria-label="Test">
-        <p>Test</p>
-        <Ul>
-          <Li>
-            <p className="paragraph">React Test Library</p>
-          </Li>
-          <Li>
-            <p className="paragraph">Jest</p>
-          </Li>
-          <Li>
-            <p className="paragraph">MSW</p>
-          </Li>
-          <Li>
-            <p className="paragraph">Storybook</p>
-          </Li>
-        </Ul>
-      </section>
+      <div className={styles.wrapper}>
+        <section className={styles.container}>
+          <span className="label">Language: </span>
+          <p className="paragraph">Javascript, typescript</p>
+        </section>
+
+        <section className={styles.container}>
+          <span className="label">Framework: </span>
+          <p className="paragraph">React.js</p>
+        </section>
+
+        <section className={styles.container}>
+          <span className="label">Bundler: </span>
+          <p className="paragraph">Webpack, Vite</p>
+        </section>
+
+        <section className={styles.container}>
+          <span className="label">Style library: </span>
+          <p className="paragraph">Emotion.js, Emotion.js</p>
+        </section>
+
+        <section className={styles.container}>
+          <span className="label">State management library: </span>
+          <p className="paragraph">Zustand, Recoil, React-Query</p>
+        </section>
+
+        <section className={styles.container}>
+          <span className="label">Test: </span>
+          <p className="paragraph">React Test Library, Jest, MSW, Storybook, Cypress</p>
+        </section>
+      </div>
     </article>
   );
 };
