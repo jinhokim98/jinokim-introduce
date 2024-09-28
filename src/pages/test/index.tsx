82cfc5aae69722bfbb6b051e6b3af1c673db8cf3
김진호 <rlawlsgh1227@gmail.com>
2024-09-28 15:41:55 +0900

    test: 카카오 테스트

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -46,7 +46,7 @@ const Test = () => {
     const account = '국민은행 030302-04-191806 10,000원';
     await window.navigator.clipboard.writeText(account);
 
-    const kakaoPayUrl = 'kakaotalk://kakaopay/sendMoney';
+    const kakaoPayUrl = 'kakaotalk://kakaopay/home';
     alert(kakaoPayUrl);
     const url = 'send?amount=10000&bank=KB국민은행&accountNo=030302-04-191806';
     window.location.href = kakaoPayUrl;
