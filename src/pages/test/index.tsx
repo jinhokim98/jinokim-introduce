1ef6eb0c452c97fac9baac79c4450b7cd174c0b5
김진호 <rlawlsgh1227@gmail.com>
2024-10-17 10:37:35 +0900

    feat: 게좌번호 dash 다 떼고 진행

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -39,7 +39,7 @@ const Test = () => {
     // const account = '국민은행 030302-04-191806 김진호 10,000원';
     // await window.navigator.clipboard.writeText(account);
 
-    const url = `supertoss://send?amount=10000&bank=${bank}&accountNo=030302-04-191806`;
+    const url = `supertoss://send?amount=10000&bank=${bank}&accountNo=03030204191806`;
     window.location.href = url;
   };
 
