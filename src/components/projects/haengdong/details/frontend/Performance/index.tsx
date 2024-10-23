d76c8aeff6d370b2c44df854b0adb32820cb7712
김진호 <rlawlsgh1227@gmail.com>
2024-10-23 20:39:46 +0900

    feat: 성능개선 포스팅 추가

--- src/components/projects/haengdong/details/frontend/Performance/index.tsx
+++ src/components/projects/haengdong/details/frontend/Performance/index.tsx
@@ -20,6 +20,27 @@ const Performance = () => {
                   text="image sprite 기법을 사용해서 이미지 파일 요청 시간을 41.85ms에서 3.66ms로 개선"
                 />
               </Li>
+              <Li>
+                <Link
+                  favicon="https://t1.daumcdn.net/tistory_admin/favicon/tistory_favicon_32x32.ico"
+                  href="https://jinokim.tistory.com/41"
+                  text="IntersectionObserver를 사용한 이미지 lazy loading으로 Loading 632ms에서 332ms로 50% 개선"
+                />
+              </Li>
+              <Li>
+                <Link
+                  favicon="https://t1.daumcdn.net/tistory_admin/favicon/tistory_favicon_32x32.ico"
+                  href="https://jinokim.tistory.com/42"
+                  text="외부 스크립트 동적 로딩을 사용해서 data transferred 761KB에서 731KB로 30KB 감소"
+                />
+              </Li>
+              <Li>
+                <Link
+                  favicon="https://t1.daumcdn.net/tistory_admin/favicon/tistory_favicon_32x32.ico"
+                  href="https://jinokim.tistory.com/43"
+                  text="코드 스플리팅과 tree shaking으로 main bundle 219KB에서 194KB로 25KB 감소"
+                />
+              </Li>
             </div>
           </div>
         </Ul>
