818dab63f898c4851f7b583e6da3d7bae36aa1c1
김진호 <rlawlsgh1227@gmail.com>
2024-12-15 19:35:59 +0900

    feat: ci 테스트

--- src/router.tsx
+++ src/router.tsx
@@ -5,21 +5,27 @@ import Test from '@pages/test';
 import App from './App';
 import ROUTER_URL from '@constants/routerUrl';
 
-const router = createBrowserRouter([
-  {
-    path: ROUTER_URL.base,
-    element: <App />,
-    children: [
-      {
-        index: true,
-        element: <Resume />,
-      },
-      {
-        path: ROUTER_URL.test,
-        element: <Test />,
-      },
-    ],
-  },
-]);
+const envValue = import.meta.env.VITE_BRANCH_NAME;
+const basename = typeof envValue !== 'undefined' ? envValue : '';
+
+const router = createBrowserRouter(
+  [
+    {
+      path: ROUTER_URL.base,
+      element: <App />,
+      children: [
+        {
+          index: true,
+          element: <Resume />,
+        },
+        {
+          path: ROUTER_URL.test,
+          element: <Test />,
+        },
+      ],
+    },
+  ],
+  {basename},
+);
 
 export default router;
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -5,3 +5,11 @@ declare module '*.svg?react' {
   const ReactComponent: React.FC<React.SVGProps<SVGElement>>;
   export default ReactComponent;
 }
+
+interface ImportMetaEnv {
+  readonly VITE_BRANCH_NAME: string | undefined;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
