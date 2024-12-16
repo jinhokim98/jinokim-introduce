12cae18a5f33ee652a942f7b731877cb2a085d25
김진호 <rlawlsgh1227@gmail.com>
2024-12-16 16:21:56 +0900

    feat: vite 설정 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import {defineConfig} from 'vite';
+import {defineConfig, loadEnv} from 'vite';
 import path from 'path';
 
 import react from '@vitejs/plugin-react';
@@ -9,16 +9,25 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), svgrPlugin()],
-  base: './',
-  resolve: {
-    alias: [
-      {find: '@assets', replacement: path.resolve(__dirname, 'src/assets')},
-      {find: '@components', replacement: path.resolve(__dirname, 'src/components')},
-      {find: '@constants', replacement: path.resolve(__dirname, 'src/constants')},
-      {find: '@pages', replacement: path.resolve(__dirname, 'src/pages')},
-      {find: '@styles', replacement: path.resolve(__dirname, 'src/styles')},
-    ],
-  },
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  let branchName = env['VITE_BRANCH_NAME'];
+
+  if (branchName === 'main' || branchName === 'develop') {
+    branchName = '';
+  }
+
+  return {
+    plugins: [react(), svgrPlugin()],
+    base: branchName,
+    resolve: {
+      alias: [
+        {find: '@assets', replacement: path.resolve(__dirname, 'src/assets')},
+        {find: '@components', replacement: path.resolve(__dirname, 'src/components')},
+        {find: '@constants', replacement: path.resolve(__dirname, 'src/constants')},
+        {find: '@pages', replacement: path.resolve(__dirname, 'src/pages')},
+        {find: '@styles', replacement: path.resolve(__dirname, 'src/styles')},
+      ],
+    },
+  };
 });
