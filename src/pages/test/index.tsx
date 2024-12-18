9d4596ed3a52ed2c1fd31dd0cc7b0939b3aea720
김진호 <rlawlsgh1227@gmail.com>
2024-12-18 13:44:51 +0900

    feat: 토스 없을 때 대처

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -18,6 +18,40 @@ const buttonStyle: React.CSSProperties = {
   color: 'white',
 };
 
+type NavigateAppArgs = {
+  android: AppUrl;
+  ios: AppUrl;
+};
+
+type AppUrl = {
+  appScheme: string;
+  storeUrl: string;
+};
+
+const navigateApp = ({android, ios}: NavigateAppArgs) => {
+  const isIOS = /iPhone|iPad|iPod/.test(navigator.userAgent);
+  const isAndroid = /Android/.test(navigator.userAgent);
+  const isMobile = isIOS || isAndroid;
+
+  if (!isMobile) {
+    alert('모바일 기기에서만 앱 실행 및 설치가 가능합니다. 모바일 환경에서 접속해 주세요.');
+    return;
+  }
+
+  const url = isIOS ? ios.appScheme : android.appScheme;
+  const storeUrl = isIOS ? ios.storeUrl : android.storeUrl;
+
+  const now = Date.now();
+  window.location.href = url;
+
+  setTimeout(() => {
+    if (Date.now() - now < 1500) {
+      // toast로 알려줘도 좋을 듯
+      window.location.href = storeUrl;
+    }
+  }, 1000);
+};
+
 const Test = () => {
   const [buttonText, setButtonText] = useState('공유하기 전');
 
@@ -36,40 +70,32 @@ const Test = () => {
   };
 
   const gobithumb = () => {
-    const now = Date.now();
-
-    const url = 'bithumb://';
-    const isIOS = /iPhone|iPad|iPod/.test(navigator.userAgent);
-    const isAndroid = /Android/.test(navigator.userAgent);
-
-    const androidStoreUrl =
-      'intent://details?id=com.btckorea.bithumb#Intent;scheme=market;package=com.android.vending;end;';
-    const iosStoreUrl = 'https://apps.apple.com/kr/app/bithumb/id1299421592?l=en-GB';
-    const storeUrl = isIOS ? iosStoreUrl : androidStoreUrl;
-
-    const isMobile = isIOS || isAndroid;
-
-    if (!isMobile) {
-      alert('모바일 기기에서만 앱 실행 및 설치가 가능합니다. 모바일 환경에서 접속해 주세요.');
-      return;
-    }
-
-    window.location.href = url;
-
-    setTimeout(() => {
-      if (Date.now() - now < 1500) {
-        // toast로 알려줘도 좋을 듯
-        window.location.href = storeUrl;
-      }
-    }, 1000);
+    navigateApp({
+      android: {
+        appScheme: 'bithumb://',
+        storeUrl: 'intent://details?id=com.btckorea.bithumb#Intent;scheme=market;package=com.android.vending;end;',
+      },
+      ios: {
+        appScheme: 'bithumb://',
+        storeUrl: 'https://apps.apple.com/kr/app/bithumb/id1299421592?l=en-GB',
+      },
+    });
   };
 
   const goToss = async () => {
     // const account = '국민은행 030302-04-191806 김진호 10,000원';
     // await window.navigator.clipboard.writeText(account);
 
-    const url = `supertoss://send?amount=10000&bank=${bank}&accountNo=03030204191806`;
-    window.location.href = url;
+    navigateApp({
+      android: {
+        appScheme: `supertoss://send?amount=10000&bank=${bank}&accountNo=03030204191806`,
+        storeUrl: 'intent://details?id=viva.republica.toss#Intent;scheme=market;package=com.android.vending;end;',
+      },
+      ios: {
+        appScheme: `supertoss://send?amount=10000&bank=${bank}&accountNo=03030204191806`,
+        storeUrl: 'https://apps.apple.com/kr/app/%ED%86%A0%EC%8A%A4/id839333328',
+      },
+    });
   };
 
   const goKakaoPay = async () => {
